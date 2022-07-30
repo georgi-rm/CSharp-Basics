da5ed7a4e43bae47082490c6f2e47afbba3ff778
georgi-rm <georgi.rm@gmail.com>
2022-07-30 12:54:55 +0300

    Problem 1 "Excellent Result" from Exercise: Conditional Statements

---
+++ T02ConditionalStatements/Lab/P01ExcellentResult/P01ExcellentResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01ExcellentResult
+{
+    class P01ExcellentResult
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if(grade >= 5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
