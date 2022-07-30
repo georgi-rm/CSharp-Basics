2adbb6b1c8343faeba2c6e43ee2e16e8d270be03
georgi-rm <georgi.rm@gmail.com>
2022-07-30 13:36:14 +0300

    Problem 2 "Greater Number" from Exercise: Conditional Statements

---
+++ T02ConditionalStatements/Lab/P02GreaterNumber/P02GreaterNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P02GreaterNumber
+{
+    class P02GreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber);
+            }
+            else
+            {
+                Console.WriteLine(secondNumber);
+            }
+        }
+    }
+}
