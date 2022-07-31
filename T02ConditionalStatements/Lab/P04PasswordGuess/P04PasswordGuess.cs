387e47c1edb5d3f8ff5badbb35df19da17a536c0
georgi-rm <georgi.rm@gmail.com>
2022-07-31 09:38:11 +0300

    Problem 4 "Password Guess" from Exercise: Conditional Statements

---
+++ T02ConditionalStatements/Lab/P04PasswordGuess/P04PasswordGuess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P04PasswordGuess
+{
+    class P04PasswordGuess
+    {
+        static void Main(string[] args)
+        {
+            const string secretPassword = "s3cr3t!P@ssw0rd";
+
+            string enteredPassword = Console.ReadLine();
+
+            if (enteredPassword == secretPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
